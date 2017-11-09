58b79c2526be03df086554cfa685b1ebbfa47d9d
rhcpancho <ANFP-08@DESKTOP-RC9I4DA>
2017-11-09 16:21:25 -0500

    Mapeo de la base de datos de usuarios

--- src/java/ec/tienda/movil/global/Propiedades.java
+++ src/java/ec/tienda/movil/global/Propiedades.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ec.tienda_movil.global;
+package ec.tienda.movil.global;
 
 import java.io.IOException;
 import java.io.InputStream;
